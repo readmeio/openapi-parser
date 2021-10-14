02f114968bec54fba68e8cf9f6091a362cda3e7c
Jon Ursenbach <jon@ursenba.ch>
2021-10-14 11:50:11 -0700

    feat: adding an option for controlling error message colorization

---
+++ test/specs/colorize-errors-option/colorize-errors-option.spec.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const { expect } = require("chai");
+const SwaggerParser = require("../../..");
+const helper = require("../../utils/helper");
+const path = require("../../utils/path");
+
+describe("`validate.colorizeErrors` option", () => {
+  it("should not colorize errors by default", async () => {
+    let parser = new SwaggerParser();
+
+    try {
+      await parser.validate(path.rel("specs/colorize-errors-option/invalid.json"));
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err).to.be.an.instanceOf(SyntaxError);
+      expect(err.message).to.match(/> 19 |             "type": "array",/);
+    }
+  });
+
+  it("should colorize errors when set", async () => {
+    let parser = new SwaggerParser();
+
+    try {
+      await parser.validate(path.rel("specs/colorize-errors-option/invalid.json"), {
+        validate: {
+          colorizeErrors: true,
+        },
+      });
+
+      helper.shouldNotGetCalled();
+    }
+    catch (err) {
+      expect(err).to.be.an.instanceOf(SyntaxError);
+      expect(err.message).to.contain("\u001b");
+    }
+  });
+});
